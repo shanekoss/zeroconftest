0566ab0115b25a3efc519439a54b276d56ed329e
Shane Koss <shane@shanekoss.com>
2021-05-12 15:53:07 -0700

    chore: commit whole android build folder

---
+++ Builds/Android/app/.cxx/cmake/release_Release/armeabi-v7a/CMakeFiles/3.10.2/CMakeASMCompiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_ASM_COMPILER "C:/Users/shane/android-sdks/ndk/22.1.7171670/toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "C:/Users/shane/android-sdks/ndk/22.1.7171670/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ar.exe")
+set(CMAKE_ASM_COMPILER_AR "CMAKE_ASM_COMPILER_AR-NOTFOUND")
+set(CMAKE_RANLIB "C:/Users/shane/android-sdks/ndk/22.1.7171670/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ranlib.exe")
+set(CMAKE_ASM_COMPILER_RANLIB "CMAKE_ASM_COMPILER_RANLIB-NOTFOUND")
+set(CMAKE_LINKER "CMAKE_LINKER-NOTFOUND")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "Clang")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
